0e0ad3b48bf83bada36c476f76acddafb9829254
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2016-06-07 17:58:41 +0300

    init

---
+++ lessons/01-setting-up/modules/Repos.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router';
+import NavLink from './NavLink'
+
+
+export default React.createClass({
+    render() {
+        return <div>
+            <h2>Repos</h2>
+            <ul>
+                <li><NavLink to="/repos/rackt/react-router">React Router</NavLink></li>
+                <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+            </ul>
+            <div>
+                {this.props.children}
+            </div>
+        </div>
+    }
+})
