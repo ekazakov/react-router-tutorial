f7685c6f433af867a2772e22148611353b612c66
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2016-06-14 01:29:01 +0300

    wip

--- lessons/01-setting-up/modules/Repos.js
+++ lessons/01-setting-up/modules/Repos.js
@@ -4,12 +4,33 @@ import NavLink from './NavLink'
 
 
 export default React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object
+    },
+
+    handleSubmit(e) {
+        e.preventDefault();
+        var userName = e.target.elements[0].value;      
+        var repo = e.target.elements[1].value;      
+        var path = `/repos/${userName}/${repo}`;
+        console.log(path);
+
+        this.context.router.push(path);
+    },
+    
     render() {
         return <div>
             <h2>Repos</h2>
             <ul>
                 <li><NavLink to="/repos/rackt/react-router">React Router</NavLink></li>
                 <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+                <li>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" placeholder="userName"/> / {' '}
+                        <input type="text" placeholder="repo"/>{' '}
+                        <button type="submit">Go</button>
+                    </form>
+                </li>
             </ul>
             <div>
                 {this.props.children}
