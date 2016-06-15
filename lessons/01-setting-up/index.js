a07bf7d81a0fa6ffd213ca9c1ec48edba759646a
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2016-06-16 00:54:51 +0300

    done main lessons

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,19 +1,7 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './modules/App'
-import About from './modules/About'
-import Repos from './modules/Repos'
-import Repo from './modules/Repo'
-import Home from './modules/Home'
+import React from 'react';
+import { render } from 'react-dom';
+import routes from './modules/routes';
 
-import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import {Router, browserHistory} from 'react-router';
 
-render(<Router history={browserHistory}>
-    <Route path="/" component={App}>
-        <IndexRoute component={Home}/>
-        <Route path="/repos" component={Repos}>
-            <Route path="/repos/:userName/:repoName" component={Repo}/>
-        </Route>
-        <Route path="/about" component={About}/>
-    </Route>
-</Router>, document.getElementById('app'))
+render(<Router history={browserHistory} routes={routes}/>, document.getElementById('app'));
--- lessons/01-setting-up/server.js
+++ lessons/01-setting-up/server.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import routes from './modules/routes';
+
 var express = require('express');
 var path = require('path');
 var compression = require('compression');
@@ -5,10 +10,34 @@ var compression = require('compression');
 var app = express();
 
 app.use(compression());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {index: false}));
 
-app.get('*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+function renderPage(appHtml) {
+    return `
+        <!doctype html public="storage">
+        <html>
+        <meta charset="utf-8"/>
+        <title>My Firest React Router Project</title>
+        <link rel="stylesheet" href="/index.css">
+        <div id="app">${appHtml}</div>
+        <script src="/bundle.js"></script>
+    `;
+}
+
+app.get('*', (req, res) => {
+    match({routes, location: req.url}, (err, redirect, props) => {
+        if (err) {
+            res.status(500).send(err.message)
+        } else if (redirect) {
+            res.redirect(redirect.pathname + redirect.search)
+        } else if (props) {
+            // hey we made it!
+            const appHtml = renderToString(<RouterContext {...props}/>);
+            res.send(renderPage(appHtml))
+        } else {
+            res.status(404).send('Not Found')
+        }
+    });
 });
 
 var PORT = process.env.PORT || 8080;
